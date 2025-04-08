89e9f484ad7af00a733e3434f47ddaa88df31d9f
abhi <abhi@qitpl.com>
2025-04-08 16:14:56 +0530

    changes

--- app/src/main/java/com/android/portal/DashboardActivity.kt
+++ app/src/main/java/com/android/portal/DashboardActivity.kt
@@ -1,4 +1,15 @@
 package com.android.portal
 
-class DashboardActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+
+
+class DashboardActivity :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dahsboard)
+
+    }
 }
--- app/src/main/java/com/android/portal/MainActivity.kt
+++ app/src/main/java/com/android/portal/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.android.portal
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
+        val navigateToDashboard = Intent(this, DashboardActivity::class.java)
+        startActivity(navigateToDashboard)
     }
 }
