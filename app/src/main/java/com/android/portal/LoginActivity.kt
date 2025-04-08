fd7cd4e7db3a6ec645c828bf3e8f4397d96b88e5
abhi <abhi@qitpl.com>
2025-04-08 16:52:07 +0530

    update

--- app/src/main/java/com/android/portal/LoginActivity.kt
+++ app/src/main/java/com/android/portal/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.android.portal
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class LoginActivity: AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var btn_sign_up = findViewById<TextView>(R.id.btn_sign_up)
+        btn_sign_up.setOnClickListener {
+            val navigateToSignup = Intent(this, SignupActivity::class.java)
+            startActivity(navigateToSignup)
+        }
     }
 }
---
+++ app/src/main/java/com/android/portal/SignupActivity.kt
@@ -0,0 +1,11 @@
+package com.android.portal;
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+public class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
