62631c8f33385c27fd5e74ce4fb6059382784138
abhi <abhi@qitpl.com>
2025-04-08 16:30:10 +0530

    chanes

--- app/src/main/java/com/android/portal/LoginActivity.kt
+++ app/src/main/java/com/android/portal/LoginActivity.kt
@@ -1,13 +1,11 @@
 package com.android.portal
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val navigateToLogin = Intent(this, LoginActivity::class.java)
-        startActivity(navigateToLogin)
+        setContentView(R.layout.activity_login)
     }
 }
